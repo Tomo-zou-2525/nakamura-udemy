d46047d4215909a6f28778caab26afe41c812554
友造-2525 <bathbook1990tomozou@gmail.com>
2020-08-20 11:07:03 +0900

    todo/削除機能追加-Bitcoin/作成

--- BitCoin/main.js
+++ BitCoin/main.js
@@ -1,9 +1,31 @@
 new Vue({
   el: "#app",
   data: {
-    bpi: null
+    bpi: null,
+    hasError: false,
+    loading: true
   },
   mounted: function() {
-    axios.get("");
-  }
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(
+        function(response) {
+          // console.log(response.data.bpi);
+          // console.log(response.data.bpi.USD.rate_float);
+          this.bpi = response.data.bpi;
+          console.log(this); //thisの値がVueかWindowに切り替わる
+        }.bind(this) //←この記述がくそ大事
+      )
+      .catch(
+        function(error) {
+          console.log(error);
+          this.hasError = true;
+        }.bind(this)
+      );
+  },
+  filters: {
+    currentDecimal(value) {
+      return value.toFixed(2);
+    }
+  } //小数点切り上げのコード
 });
--- todo/todo.js
+++ todo/todo.js
@@ -7,10 +7,17 @@ new Vue({
   methods: {
     addTodo: function(event) {
       // alert("うほうほ");
+      if (this.newItem == "") return; //空文字のヴァリデーション
       let todo = {
-        item: this.newItem
+        item: this.newItem,
+        isDone: false
       };
       this.todos.push(todo);
+      this.newItem = "";
+    },
+    deleteTodo: function(index) {
+      alert("消しまっせ", index);
+      this.todos.splice(index, 1);
     }
   }
 });
