1d28ff47e5907ea6ea03fa124456cdc48640f497
友造-2525 <bathbook1990tomozou@gmail.com>
2020-08-24 22:32:20 +0900

    動的クラス

---
+++ data-vinding/応用　動的クラス/例題.js
@@ -0,0 +1,9 @@
+new Vue({
+  el: "#app",
+  data: {},
+  methods: {
+    alert: function() {
+      alert("できるかな？");
+    }
+  }
+});
--- realtime_search/js/main.js
+++ realtime_search/js/main.js
@@ -1,3 +1,44 @@
 var app = new Vue({
-    el: '#app'
-})
+  el: "#app",
+  data: {
+    items: null,
+    keyword: "",
+    message: ""
+  },
+  watch: {
+    keyword: function(newKeyword, oldkeyword) {
+      // console.log(newKeyword);
+      this.message = "ちょっとまつがよい";
+      this.debouncedGetAnswer();
+    }
+  },
+  methods: {
+    getAnswer: function() {
+      if (this.keyword === "") {
+        this.items = null;
+        this.message = "";
+        return;
+      }
+      this.message = "Loading...";
+      let vm = this;
+      let params = { page: 1, per_page: 20, query: this.keyword };
+      axios
+        .get("https://qiita.com/api/v2/items", { params })
+        .then(function(response) {
+          console.log(response);
+          vm.items = response.data;
+        })
+        .catch(function(error) {
+          vm.message = "Error!" + error;
+        })
+        .finally(function(error) {
+          vm.message = "";
+        });
+    }
+  },
+  created: function() {
+    // this.keyword = "javaScript";
+    // this.getAnswer();
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000);
+  }
+});
